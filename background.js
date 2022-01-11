faceb4288a2d8f4848300bc3d52b27b519e7ca6e
mbeckwithfl <97210199+mbeckwithfl@users.noreply.github.com>
2022-01-11 01:09:10 -0700

    1st post to webhook!

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 //create a context menu on installation that appears when right-clicking an image
 chrome.runtime.onInstalled.addListener(() => {  
-  
   chrome.contextMenus.create({
       title: "Image2Discord",
       id: "image-2-discord",
@@ -8,13 +7,21 @@ chrome.runtime.onInstalled.addListener(() => {
     });
   });
   
-  //add listener for clicking context menu item. Define which context menu item this applies to.
-  //open a new chrome tab to google as a test
-  chrome.contextMenus.onClicked.addListener(function(info) {
-
-    if (info.menuItemId === "image-2-discord") {
-
-      chrome.tabs.create({url: "https://www.google.com"})
-  
+  //add listener for clicking context menu item.
+chrome.contextMenus.onClicked.addListener(function(info) {
+   //Define which context menu item this applies to.
+  if (info.menuItemId == "image-2-discord") {
+    //create webhook url variable. This will be updated so webhooks can be input through the chrome extension UI.
+    const whurl ="https://discord.com/webhook/goes/here"
+    //create message variable. This will be updated so message variable is created on click.
+    const msg = {
+    "content": "https://image/URL/goes/here"
   }
-})
+//manifest V3 uses fetch() API where V2 used XMLHttpRequest. This also converts the JS object const msg to a JSON string for interpretation by the webhook.
+  fetch(whurl + "?wait=true", {
+    "method":"POST", 
+    "headers": {"content-type": "application/json"}, 
+    "body": JSON.stringify(msg)}
+       )}
+});
+//we are talking to Discord!!!
