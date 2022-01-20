b1960a311ae16b39dcbad1bd1ce65c1bdcadd490
mbeckwithfl <97210199+mbeckwithfl@users.noreply.github.com>
2022-01-19 23:13:36 -0700

    Added UI.
    UI currently supports creation of a new context menu item through input in settings popup window. 
    
    Next: 
    -Create one list of channel names and one list of webhook URLs in Chrome storage API. 
    -Add function to append new channel to lists on UI "link channel" click. 
    -Add function to iterate through both lists and send object to correct discord channel on context menu item click.

--- background.js
+++ background.js
@@ -1,19 +1,15 @@
-//create a context menu on installation that appears when right-clicking an image
 chrome.runtime.onInstalled.addListener(() => {  
-  chrome.contextMenus.create({
-      title: "Image2Discord",
-      id: "image-2-discord",
-      contexts: ["image"],
-    });
+  const listofchannelnames = []
+  const listofwebhooks = []
+  chrome.storage.sync.set({namelistforadding : listofchannelnames})
+  chrome.storage.sync.set({whlistforadding : listofwebhooks})
   });
-  
   //add listener for clicking context menu item.
 chrome.contextMenus.onClicked.addListener(function(info) {
    //Define which context menu item this applies to.
   if (info.menuItemId == "image-2-discord") {
     //webhook url variable. This will be updated so webhooks can be input through the extension UI.
-    console.log(info.srcUrl)
-    const whurl ="https://discord.com/api/webhooks/930195632675766343/EYo0SQV21fA9rxETrUZ8bYTdh4OW-5uTjLgnDASV-piis5aYPuWKZOGente549yovw1n"
+    const whurl ="https://discord.com/api/webhooks/931459626707320844/HfTswb5JWehvhQrYNaDCtN9k_a5wnf5gQpV6dng_1a0WLYc3lptnKkPGmR0VYJt9y3vb"
     const testUrl = info.srcUrl
     const msg ={ 
       "embeds": [{
@@ -29,3 +25,4 @@ chrome.contextMenus.onClicked.addListener(function(info) {
   "body": JSON.stringify(msg)})
   }
 });
+
