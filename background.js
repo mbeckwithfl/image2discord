bdfbfc6e68c65d401b918775d9b2a988cdc66119
Matt Beckwith <mbeckwithfl@yahoo.com>
2022-01-21 09:25:02 -0700

    Additional UI & removal features
    -HTML options page
    -Forms to remove context menu items that are no longer in use
    -will add CSS for pretty

--- background.js
+++ background.js
@@ -1,28 +1,25 @@
-chrome.runtime.onInstalled.addListener(() => {  
-  const listofchannelnames = []
-  const listofwebhooks = []
-  chrome.storage.sync.set({namelistforadding : listofchannelnames})
-  chrome.storage.sync.set({whlistforadding : listofwebhooks})
-  });
-  //add listener for clicking context menu item.
-chrome.contextMenus.onClicked.addListener(function(info) {
-   //Define which context menu item this applies to.
-  if (info.menuItemId == "image-2-discord") {
-    //webhook url variable. This will be updated so webhooks can be input through the extension UI.
-    const whurl ="https://discord.com/api/webhooks/931459626707320844/HfTswb5JWehvhQrYNaDCtN9k_a5wnf5gQpV6dng_1a0WLYc3lptnKkPGmR0VYJt9y3vb"
-    const testUrl = info.srcUrl
-    const msg ={ 
-      "embeds": [{
-        "image": {
-          "url": testUrl
-        }
-      }]
-    }
-//Post to webhook via fetch API
-  fetch(whurl + "?wait=true", 
-  {"method":"POST", 
-  "headers": {"content-type": "application/json"},
-  "body": JSON.stringify(msg)})
-  }
-});
-
+chrome.contextMenus.onClicked.addListener( async function(info) {
+  
+  const id = "https://discord.com/api/webhooks/933803837893017610/i0DG5XtYl_fQrmapWDLqTF9jEJ5U5XfKkqSRbkQ5gxtQ3KQr9sgSlq2o6B8xNnUSe3OQ"
+    chrome.contextMenus.remove({
+        menuItemId: id,
+    },
+  console.log(id),
+  console.log(info.menuItemId)
+    )
+  
+  // console.log(info.menuItemId)  
+  // const imgSrcUrl = info.srcUrl
+  //   const whurl =info.menuItemId
+  //   const msg ={ 
+  //     "embeds": [{
+  //       "image": {
+  //         "url": imgSrcUrl
+  //       }
+  //     }]
+  //   }
+  // fetch(whurl + "?wait=true", 
+  // {"method":"POST", 
+  // "headers": {"content-type": "application/json"},
+  // "body": JSON.stringify(msg)})
+})
