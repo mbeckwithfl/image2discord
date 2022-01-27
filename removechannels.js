f7cd07e28402c1af59cca8cab4ab479ea87ed0db
Matt Beckwith <mbeckwithfl@yahoo.com>
2022-01-26 18:32:43 -0700

    Update removechannels.js

--- removechannels.js
+++ removechannels.js
@@ -1,11 +1,15 @@
-function removeOne(){
-    const id = document.getElementById('webhookforRemoval').value
-    chrome.contextMenus.remove(id)
+function removeOneChannel(){
+    //To remove context menu item, Chrome API needs the MenuItemID of the context menu item from when it was created.
+    //Input the webhookURL of the channel you want to remove
+    const idOfChannel = document.getElementById('webhookforRemoval').value
+    chrome.contextMenus.remove(idOfChannel)
     alert("Channel removed!")
 }
+//Nothing needs to be passed to remove all the context menu items.
 function removeAll(){
     chrome.contextMenus.removeAll()
     alert("All channels removed!")
 }
-document.getElementById('removeonechannel').onclick = removeOne
+/Tie the functions to the appropriate HTML buttons
+document.getElementById('removeonechannel').onclick = removeOneChannel
 document.getElementById('removeallchannels').onclick = removeAll
