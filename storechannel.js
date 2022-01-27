1579f44b6b419ee739aa6d2a409e165364560c46
Matt Beckwith <mbeckwithfl@yahoo.com>
2022-01-26 18:52:19 -0700

    Update storechannel.js

--- storechannel.js
+++ storechannel.js
@@ -1,43 +1,31 @@
-async function channelAdd(){
+function channelAdd(){
+  //Get desired name and webhook from HTML UI
   channelNameInput = document.getElementById('channelname').value
   webhookInput = document.getElementById('webhookurl').value
-  chrome.storage.sync.set({channelTitle: channelNameInput})
-  chrome.storage.sync.set({webHook: webhookInput})
-  var thing1 = new Promise(function(resolve, reject){
-    chrome.storage.sync.get(['channelTitle'], function(info){
-        resolve(info.channelTitle);
-    })
-  });
-  var thing2 = new Promise(function(resolve, reject){
-    chrome.storage.sync.get(['webHook'], function(info){
-        resolve(info.webHook);
-    })
-  });
-  const nameyName = await thing1
-  const hookyHook = await thing2
-  console.log(nameyName)
-  console.log(hookyHook)
-
-  chrome.contextMenus.create({
-    title: nameyName,
-    id: nameyName,
-    contexts: ["image"], 
-  })
- 
-  var thing3 = new Promise(function(resolve, reject){
-    chrome.storage.sync.get(['namelistforadding'], function(info){
-           resolve(info.namelistforadding); 
-    })
-  });
-  var thing4 = new Promise(function(resolve, reject){
-    chrome.storage.sync.get(['whlistforadding'], function(info){
-           resolve(info.whlistforadding); 
-    })
-  });
-  const listyList1 = await thing3
-  const listyList2 = await thing4
-  console.log(listyList1)
-  console.log(listyList2)
-
+  //Set the name and webhook to an object, that also includes the context when the menu item should pop (images)
+  contextMenuItem = {
+    title: channelNameInput,
+    id: webhookInput,
+    contexts: ["image"]
+  }
+  //create the contextmenuitem
+  chrome.contextMenus.create(contextMenuItem)
+  //clear the forms and alert if improper information was input
+  function clear(data) {
+    document.getElementById("channelname").value = data;
+    document.getElementById("webhookurl").value = data;
+  }  
+  if(webhookInput.length == 0 && channelNameInput.length == 0){
+    alert("Please input a channel name & a webhook URL.")
+    return
+  } else if (channelNameInput.length > 0 && webhookInput.length == 0) {
+    alert("Please enter a Discord webhook URL")
+    return
+  } else if (webhookInput.length != 120){
+    alert("That's not a Discord webhook.")
+  }else if (webhookInput.length = 120){
+    alert("Channel added!")
+  }
+clear("")
 }
 document.getElementById('storechannel').onclick = channelAdd
