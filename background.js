449da50f61a500e27ca13a13c4b3e2bd9081d114
Matt Beckwith <mbeckwithfl@yahoo.com>
2022-01-26 18:24:20 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,18 @@
-chrome.contextMenus.onClicked.addListener(function(info) {
-  
-  const id = info.menuItemId
-  console.log(info.menuItemId)  
-  const imgSrcUrl = info.srcUrl
-    const whurl =info.menuItemId
+chrome.contextMenus.onClicked.addListener(function(info) { 
+ //get the srcURL of the image that was right-clicked
+  const imageSrcUrl = info.srcUrl
+ //get the webhook URL that was set to the context menu item on creation
+  const webhookurl =info.menuItemId
+ //an object in the format for Discord API, containing the srcURL
     const msg ={ 
       "embeds": [{
         "image": {
-          "url": imgSrcUrl
+          "url": imageSrcUrl
         }
       }]
     }
-  fetch(whurl + "?wait=true", 
+  //send via Fetch API to the the webhookURL of the context menu item that was clicked.
+  fetch(webhookurl + "?wait=true", 
   {"method":"POST", 
   "headers": {"content-type": "application/json"},
   "body": JSON.stringify(msg)})
